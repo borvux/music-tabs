ce266cb9a4f26f84bd40ff2e0ae064aae9af6260
borvux <bennyjoram@email.com>
2024-07-22 11:55:35 -0500

    added assoication for users and tabs models

--- app/models/tab.rb
+++ app/models/tab.rb
@@ -10,4 +10,6 @@
 #  user_id    :integer
 #
 class Tab < ApplicationRecord
+  # belongs_to :user, required: true, class_name: "User", foreign_key: "user_id"
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # has_many  :tabs, class_name: "Tab", foreign_key: "user_id", dependent: :destroy
+  has_many  :tabs, dependent: :destroy
 end
