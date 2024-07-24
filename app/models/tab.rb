743a101f011b9c63ae249d542cc903d8855100ce
borvux <bennyjoram@email.com>
2024-07-24 10:54:49 -0500

    added the rich text to the forms and tabs

--- app/models/tab.rb
+++ app/models/tab.rb
@@ -12,5 +12,7 @@
 class Tab < ApplicationRecord
   # belongs_to :user, required: true, class_name: "User", foreign_key: "user_id"
   belongs_to :user
+
+  # needed to change it to a trix text editor 
   has_rich_text :content
 end
