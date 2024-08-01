22d8d2bfb6c05221b229a7967fe876e00666cf7e
borvux <bennyjoram@email.com>
2024-08-01 16:27:46 -0500

    trying to get rid of rich_text error

--- db/migrate/20240724153620_remove_body_from_tabs.rb
+++
@@ -1,5 +0,0 @@
-class RemoveBodyFromTabs < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :tabs, :body, :string
-  end
-end
--- db/migrate/20240724153953_add_content_to_tabs.rb
+++
@@ -1,5 +0,0 @@
-class AddContentToTabs < ActiveRecord::Migration[7.1]
-  def change
-    add_column :tabs, :content, :rich_text
-  end
-end
