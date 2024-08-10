a4800e75dea3c926163d6a17103ef6581db319fe
borvux <bennyjoram@email.com>
2024-08-09 21:13:38 -0500

    added modals to the buttons and links

--- app/javascript/link_modal.js
+++ app/javascript/link_modal.js
@@ -1,11 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // Select the modals
   var deleteModal = document.getElementById('confirmationModal');
-  deleteModal.addEventListener('show.bs.modal', function (event) {
-    var button = event.relatedTarget;
-    var tabId = button.getAttribute('data-bs-tab-id');
+  var logoutModal = document.getElementById('logoutConfirmationModal');
 
-    // Set the correct URL for the delete action in the confirm button
-    var confirmDeleteButton = document.getElementById('confirm-delete');
-    confirmDeleteButton.href = `/tabs/${tabId}`;
-  });
+  if (logoutModal) {
+    logoutModal.addEventListener('show.bs.modal', function (event) {
+      var button = event.relatedTarget; // Button that triggered the modal
+      var path = button.getAttribute('data-path');
+      
+      // Set the correct URL for the logout action in the confirm button
+      var confirmLogoutButton = logoutModal.querySelector('#confirm-action');
+      if (confirmLogoutButton) {
+        confirmLogoutButton.setAttribute('href', path);
+      }
+    });
+  } else if (deleteModal) {
+    deleteModal.addEventListener('show.bs.modal', function (event) {
+      var button = event.relatedTarget; // Button that triggered the modal
+      var tabId = button.getAttribute('data-bs-tab-id');
+      
+      // Set the correct URL for the delete action in the confirm button
+      var confirmDeleteButton = deleteModal.querySelector('#confirm-action');
+      if (confirmDeleteButton) {
+        confirmDeleteButton.setAttribute('href', `/tabs/${tabId}`);
+      }
+    });
+  }
 });
